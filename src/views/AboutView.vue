34f49103a69cca59b70b4c4cad1c85348b30da54
Andreas Huygelen <andreas.huygelen@student.ap.be>
2023-11-22 13:52:48 +0100

    Added weather api

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,40 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      location: 'Mortsel',
+      weatherData: null,
+      apiKey: import.meta.env.VITE_API_URL
+      
+    };
+    
+  },
+  
+  methods: {
+    async getWeather() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${this.apiKey}`);
+        this.weatherData = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+}
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <input type="text" v-model="location" placeholder="Voer locatie in" @keyup.enter="getWeather">
+    <button @click="getWeather">Zoek weer</button>
+
+    <div v-if="weatherData">
+      <h2>{{ weatherData.name }}</h2>
+      <p>{{ weatherData.weather[0].description }}</p>
+      <p>Temperatuur: {{ weatherData.main.temp }}°C</p>
+      <!-- Andere relevante weerinformatie weergeven -->
+    </div>
   </div>
 </template>
 
